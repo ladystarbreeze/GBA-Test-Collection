b72227c4c80200cfd4ccce41482b35ea6795c22e
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2021-08-03 20:00:31 +0200

    Update entrypoint.asm
    Fixed: indentation

--- source/entrypoint.asm
+++ source/entrypoint.asm
@@ -66,37 +66,37 @@ EntryPoint:
     ldmia     r0, {r0-r2}
     bl        MemCpy32
 	
-	ldr       r11, [r12, 0x28]
+    ldr       r11, [r12, 0x28]
 
     ; clear EWRAM
     add       r0, r12, 0x2C
     ldmia     r0, {r0-r2}
     mov       lr, pc
-	bx        r11
+    bx        r11
 
     ; clear IWRAM
     add       r0, r12, 0x38
     ldmia     r0, {r0-r2}
     mov       lr, pc
-	bx        r11
+    bx        r11
 
     ; clear Palette RAM
     add       r0, r12, 0x44
     ldmia     r0, {r0-r2}
     mov       lr, pc
-	bx        r11
+    bx        r11
 
     ; clear Video RAM
     add       r0, r12, 0x50
     ldmia     r0, {r0-r2}
     mov       lr, pc
-	bx        r11
+    bx        r11
 
     ; clear Object RAM
     add       r0, r12, 0x5C
     ldmia     r0, {r0-r2}
     mov       lr, pc
-	bx        r11
+    bx        r11
 
     ; jump to Main()
     bl        Main
@@ -107,15 +107,15 @@ EntryPoint:
     .Pool_EntryPoint:
         ; stack pointers (IRQ, SVC, SYS) [0x00]
         dw        MEM_IRAM + 0x7FA0, MEM_IRAM + 0x7FE0, MEM_IRAM + 0x7F00
-		; WAITCNT [0x0C]
-		dw        0x4317
+        ; WAITCNT [0x0C]
+        dw        0x4317
         ; MemCpy32 - copies MemCpy32 to IWRAM + 00h [0x10]
-		dw        MEM_IRAM + 0x00, MEM_ROM0 + MemCpy32, Size_MemCpy32 / 4
+        dw        MEM_IRAM + 0x00, MEM_ROM0 + MemCpy32, Size_MemCpy32 / 4
         ; MemCpy32 - copies MemSet32 to IWRAM + 20h [0x1C]
-		dw        MEM_IRAM + 0x20, MEM_ROM0 + MemSet32, Size_MemSet32 / 4
-		; MemSet32 address [0x28]
-		dw        Addr_MemSet32
-		; MemSet32 - clear EWRAM [0x2C]
+        dw        MEM_IRAM + 0x20, MEM_ROM0 + MemSet32, Size_MemSet32 / 4
+        ; MemSet32 address [0x28]
+        dw        Addr_MemSet32
+        ; MemSet32 - clear EWRAM [0x2C]
         dw        MEM_ERAM, 0, Size_MEM_ERAM / 4
         ; MemSet32 - clear IWRAM [0x38]
         dw        Addr_MemSet32 + Size_MemSet32, 0, (Size_MEM_IRAM - (Size_MemCpy32 + Size_MemSet32)) / 4
